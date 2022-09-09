ac5ddba34584a8434675d89bed2ca88cc40fc232
Onwuka Ngozi <you@example.com>
2022-09-09 10:22:39 -0700

    If else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,14 +4,12 @@
 
 /* more headers goes there */
 
-/* betty style doc for function main goes there 
+/**
+ * betty style doc for function main goes there 
  * main -print the string in put function
- *
- * Description: using the main function
- * 
- *this program print "if statement of n value randomly picked"
- *
- *  Return: 0
+ * * Description: using the main function
+ * *this program print "if statement of n value randomly picked"
+ * *  Return: 0
  * */
 
 int main(void)
@@ -23,15 +21,15 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n<0)
 {
-	printf("%d is negative \n", n);
+	printf("%d is negative\n", n);
 }
 else if (n==0)
 {
-	printf("%d is zero \n", n);
+	printf("%d is zero\n", n);
 }
 else if (n>0)
 {
-	printf("%d is positive \n", n);
+	printf("%d is positive\n", n);
 }
 return (0);
 }
