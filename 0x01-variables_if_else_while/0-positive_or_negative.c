45677e50f8beba8e2432125757fc005874876879
Onwuka Ngozi <you@example.com>
2022-09-09 16:10:07 -0700

    Random n value

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,13 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
-/* more headers goes there */
-
-/* *
- * betty style doc for function main goes there 
+/**
  * main -print the string in put function
- * * Description: using the main function
- * * this program print "if statement of n value randomly picked"
+ * Description: using the main function
+ * this program print "if statement of n value randomly picked"
  * Return: 0
  */
 int main(void)
@@ -17,7 +14,6 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-/* your code goes there */
 if (n < 0)
 {
 	printf("%d is negative\n", n);
