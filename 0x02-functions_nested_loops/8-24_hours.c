e38fa1640d2a121b8aa11f6921d9989b51b7a1d0
Onwuka Ngozi <you@example.com>
2022-09-13 19:47:31 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,10 +2,10 @@
 /**
  * jack_bauer - function prints every minutes of the day of Jack Bauer, start1
  * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
+ * and resets mins
  * Return: 0
  *
  */
-
 void jack_bauer(void)
 {
 	int hours = 0;
@@ -15,18 +15,19 @@ void jack_bauer(void)
 
 	while (hours <= 23)
 	{
-		while (minutes <= 59)
-		{
-			hours_remainder = hours % 10;
-			_putchar(hours / 10 + '0');
-			_putchar(hours_remainder + '0');
-			_putchar(':');
-			_putchar(minutes / 10 + '0');
-			_putchar(mins_remainder + '0');
-			minutes++;
-			_putchar('\n');
-		}
-		hours++;
-		minutes = 0;
+	while (minutes <= 59)
+	{
+	mins_remainder = minutes % 10;
+	hours_remainder = hours % 10;
+	_putchar(hours / 10 + '0');
+	_putchar(hours_remainder + '0');
+	_putchar(':');
+	_putchar(minutes / 10 + '0');
+	_putchar(mins_remainder + '0');
+	minutes++;
+	_putchar('\n');
+	}
+	hours++;
+	minutes = 0;
 	}
 }
