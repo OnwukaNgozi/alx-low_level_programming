f31029f21f0e8a0822bf3ed08a16cc746a2b7b57
Onwuka Ngozi <you@example.com>
2022-09-13 20:24:49 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,12 +16,11 @@ void times_table(void)
 			_putchar(' ');
 
 			prod = num * mult;
+
 			if (prod <= 0)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
-
-			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
