dadaedc1163d3bcca16cf785e5349757838d6fcb
Onwuka Ngozi <you@example.com>
2022-09-16 07:32:54 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,20 +12,21 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
-			printf("FizzBuzz ");
-		else if (n % 3 == 0)
-			printf("Fizz ");
-		else if (n % 5 == 0)
-		{
-			if (num != 100)
-			{
-				printf("Buzz ");
-			}else
-				printf("Buzz");
-		}
-		else
-			printf("n "; ('\n'));
+
+	if ((n % 3 == 0) && (n % 5 == 0))
+		printf("FizzBuzz");
+	else if (n % 5 == 0)
+		printf("Buzz");
+	else if (n % 3 == 0)
+		printf("Fizz");
+	else
+		printf("%i", n);
+	
+	if (n < 100)
+		printf(" ");
+	else
+		printf("\n");
+	
 	}
 	return (0);
 }
