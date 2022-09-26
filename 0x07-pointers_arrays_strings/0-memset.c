5f25221c0e270fb727f00d5affe714385ab79803
Onwuka Ngozi <you@example.com>
2022-09-26 16:45:36 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_memset - fills a memory lock with a constant byte
+*@s: address to memory block
+*@b: char to be used
+*@n: number of bytes to be used
+*
+*Return: pointer to the memory lock
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n)
+	{
+		s[n - 1 ] = b;
+		n--;
+	}
+	return (s);
+}
