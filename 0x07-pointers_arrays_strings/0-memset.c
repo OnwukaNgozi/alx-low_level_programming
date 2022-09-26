708cd08768af6f4d8fa3c40e5c241cdf882f6969
Onwuka Ngozi <you@example.com>
2022-09-26 16:47:48 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-*_memset - fills a memory lock with a constant byte
-*@s: address to memory block
-*@b: char to be used
-*@n: number of bytes to be used
+* _memset - fills a memory lock with a constant byte
+* @s: address to memory block
+* @b: char to be used
+* @n: number of bytes to be used
 *
-*Return: pointer to the memory lock
+* Return: pointer to the memory lock
 */
 
 char *_memset(char *s, char b, unsigned int n)
