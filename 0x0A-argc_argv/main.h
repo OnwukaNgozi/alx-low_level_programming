ded820b4d140f991dc6835ea7e0be224248368c2
Onwuka Ngozi <you@example.com>
2022-09-30 08:22:17 -0700

    main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,16 +1,2 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
-/**
-* main - contains the variable function
-* prototype - contains prototype for all tasks
-* 
-* Return - 0
-*/
-
 int _putchar(char c);
 int atoi(char j[]);
-
-#endif /*MAIN_H*/
