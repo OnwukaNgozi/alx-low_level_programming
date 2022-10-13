1b5f3ceb19cf85a1093a1e1cbc61358dbbc0e9e6
Onwuka Ngozi <you@example.com>
2022-10-13 08:48:33 -0700

    Write a function that prints strings, followed by a new line.

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,7 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *str;
 	va_list valist;
 
-	va_list(valist, n);
+	va_start(valist, n);
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(valist, char *);
